485ee7c6e70a0a9832ab4e4d6c9f9fc8243f242b
LinJinxing <172601673@qq.com>
2017-09-20 17:59:18 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -5,31 +5,51 @@ function _(s) {
 // 获取audioContext对象
 var audioContext = new(window.AudioContext || window.webkitAudioContext)()
 // 获取gainNode对象(可调节音量)
-var gain = audioContext[audioContext.createGain? "createGain":"createGainNode"]()
+var gain = audioContext[audioContext.createGain ? "createGain" : "createGainNode"]()
 // 获取analyser对象(可分析音频数据)
 var analyser = audioContext.createAnalyser()
 // 创建联系
 gain.connect(audioContext.destination)
 analyser.connect(gain)
 
-// 获取音频
+// ajax对象
 var xhr = new XMLHttpRequest()
+// 当前的音乐播放源
 var curSource = null
-function loadData(url){
+// 获取音频
+function loadData(url) {
     xhr.open("GET", url, true)
     xhr.responseType = "arraybuffer"
     xhr.onload = function () {
-        audioContext.decodeAudioData(xhr.response, function(buffer){
+        audioContext.decodeAudioData(xhr.response, function (buffer) {
             var bufferSource = audioContext.createBufferSource()
             bufferSource.buffer = buffer
             bufferSource.connect(analyser)
-            curSource && curSource[curSource.stop? "stop":"noteOff"]()
+            /**
+             * bufferSource => analyser => gain => destination
+             */
+            // 停止上一个的播放
+            curSource && curSource[curSource.stop ? "stop" : "noteOff"]()
             curSource = bufferSource
-            curSource && curSource[curSource.start? "start":"noteOn"](0)
-        }, function(err){
+            curSource && curSource[curSource.start ? "start" : "noteOn"](0)
+        }, function (err) {
             return err
         })
     }
     xhr.send()
 }
-loadData("./src/李想Evelyn - 分赃.mp3")
+// 加载和播放音乐
+// loadData("./src/李想Evelyn - 分赃.mp3")
+function visual() {
+    var arr = new Uint8Array(analyser.frequencyBinCount)
+    var requestAnimationFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        mozRequestAnimationFrame
+    requestAnimationFrame(start)
+    function start(){
+        analyser.getByteFrequencyData(arr)
+        console.log(arr)
+        requestAnimationFrame(strat)
+    }
+}
+visual()
