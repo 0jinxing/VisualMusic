0dcbf7966a5bc973f2c29cfa75f9f25806ef0551
LinJinxing <172601673@qq.com>
2017-09-21 21:15:06 +0800

    github的效果有差异,不可能的

--- js/index.js
+++ js/index.js
@@ -13,34 +13,8 @@ analyser.fftSize = 512
 gain.connect(audioContext.destination)
 analyser.connect(gain)
 
-// ajax对象
-var xhr = new XMLHttpRequest()
 // 当前的音乐播放源
 var curSource = null
-// 获取音频
-function loadData(url) {
-    xhr.open("GET", url, true)
-    xhr.responseType = "arraybuffer"
-    xhr.onload = function () {
-        audioContext.decodeAudioData(xhr.response, function (buffer) {
-            var bufferSource = audioContext.createBufferSource()
-            bufferSource.buffer = buffer
-            bufferSource.connect(analyser)
-            /**
-             * bufferSource => analyser => gain => destination
-             */
-            // 停止上一个的播放
-            curSource && curSource[curSource.stop ? "stop" : "noteOff"]()
-            curSource = bufferSource
-            curSource && curSource[curSource.start ? "start" : "noteOn"](0)
-        }, function (err) {
-            return err
-        })
-    }
-    xhr.send()
-}
-// 加载和播放音乐
-loadData("./src/李想Evelyn - 分赃.mp3")
 
 // 选择文件播放
 var fileInput = _("#file-input")[0]
